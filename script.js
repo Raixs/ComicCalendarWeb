b07d0bbe6d18f2ceb39dd2a58733e9dfd12ffa90
Hans Pereira <hanspereira63@gmail.com>
2024-08-13 09:23:06 +0200

    feat: Agregar información adicional en tarjeta de eventos

--- script.js
+++ script.js
@@ -132,7 +132,10 @@ function displayEvents(events, append = false) {
                 <div class="card-body">
                     <h5 class="card-title">${event.summary}</h5>
                     <p class="card-text"><i class="fas fa-calendar-alt"></i> <strong>Fecha:</strong> ${event.start_date} - ${event.end_date}</p>
+                    <p class="card-text"><i class="fas fa-map-marker-alt"></i> <strong>Ciudad:</strong> ${event.city}</p>
                     <p class="card-text"><i class="fas fa-map-marker-alt"></i> <strong>Provincia:</strong> ${event.province}</p>
+                    <p class="card-text"><i class="fas fa-landmark"></i> <strong>Comunidad:</strong> ${event.community}</p>
+                    <p class="card-text"><i class="fas fa-tag"></i> <strong>Tipo:</strong> ${event.type}</p>
                     <p class="card-text"><i class="fas fa-map-pin"></i> <strong>Dirección:</strong> ${event.address}</p>
                     <p class="card-text event-description"><i class="fas fa-info-circle"></i> <strong>Descripción:</strong> ${event.description}</p>
                     ${event.description.length > 200 ? `<span class="show-more">Mostrar más</span>` : ''}
@@ -191,7 +194,7 @@ function showAlert(message, type = 'info') {
 
     setTimeout(() => {
         $(alert).alert('close');
-    }, 1000); // El mensaje desaparecerá después de 3 segundos
+    }, 1000); // El mensaje desaparecerá después de 1 segundos
 }
 
 function showLoading() {
